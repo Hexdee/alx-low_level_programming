d20fcdb1e350c4f9d79cf78d465a499c9510c2b6
Hexdee <lukmansaheed79@gmail.com>
2022-03-10 07:42:23 -0800

    advance 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+*/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return 1;
+}
