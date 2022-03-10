0ad89f72eef67f1fa7d46aecc3a2bfa76e07be22
Hexdee <lukmansaheed79@gmail.com>
2022-03-10 09:04:48 -0800

    advance 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\"");
-	fprintf(stderr, "- Dora Korpar, 2015-10-19\n");
-	return (1);
+	fprintf(stderr, "- Dora Korpar, 2015-10-19\n", 58);
+	return (0);
 }
