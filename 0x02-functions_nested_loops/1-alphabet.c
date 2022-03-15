63f75ffd44b12b24cd17c1f133e19937224648db
Hexdee <lukmansaheed79@gmail.com>
2022-03-15 18:38:34 +0100

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ * print_alphabet - entry point
+ *
+ * Return: void
+*/
+void print_alphabet(void)
+{
+	for (int i = 'a'; i < 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
