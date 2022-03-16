6f7dcae348d490f6c666e27a764d095309b695c7
Hexdee <lukmansaheed79@gmail.com>
2022-03-16 14:54:43 +0100

    did advance 3

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: a program that prints the sum of
+ * even term values of fibonacci series less
+ * than 4,000,000
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	long int prev1, prev2, nextNum,  sum;
+
+	prev1 = 0;
+	prev2 = 1;
+	nextNum = 0;
+	sum = 0;
+	while (nextNum < 4000000)
+	{
+		nextNum = prev1 + prev2;
+		if (nextNum % 2 == 0)
+		{
+			sum += nextNum;
+		}
+		prev1 = prev2;
+		prev2 = nextNum;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
