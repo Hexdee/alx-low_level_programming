0f171f43032ff7192bdf1449265b28465502fc88
Hexdee <lukmansaheed79@gmail.com>
2022-03-15 17:31:25 -0700

    did 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * print_last_digit - a function return the last digit of a numbee
+ * print_last_digit - a function return the last digit of a number
  * @n: number to check the last digit
  * Return: the last digit of n
  */
 
 int print_last_digit(int n)
 {
+	_putchar(n % 10);
 	return (n % 10);
 }
