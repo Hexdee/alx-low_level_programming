50671387332a9e7f954bcb554ebb04e9aec1ae08
hexdee <lukmansaheed79@gmail.com>
2022-03-21 21:31:36 +0000

    did 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * puts2 - a function that prints every other
+ * character of a string, starting with the
+ * first character, followed by a new line.
+ * @str: The string to print
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+}
