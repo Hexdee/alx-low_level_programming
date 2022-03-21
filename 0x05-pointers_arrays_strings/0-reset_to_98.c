11ea468e0284762309d7374877246016fce3d536
Hexdee <lukmansaheed79@gmail.com>
2022-03-21 16:52:06 +0100

    did 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+/** reset_to_98 - A function that takes a pointer to an
+* int as parameter and updates the value it points to to 98.
+*@n: pointer to the int to be modified
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
