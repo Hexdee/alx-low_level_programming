deee74e1df1811a151ab932f2c811c3148aea364
Hexdee <lukmansaheed79@gmail.com>
2022-03-21 16:53:37 +0100

    did 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,5 @@
-/** reset_to_98 - A function that takes a pointer to an
+/**
+*  reset_to_98 - A function that takes a pointer to an
 * int as parameter and updates the value it points to to 98.
 *@n: pointer to the int to be modified
 */
