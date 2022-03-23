984eee652cf5f71bf606c51148b7a073918ff608
hexdee <lukmansaheed79@gmail.com>
2022-03-23 18:21:42 +0000

    initial commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -18,9 +18,9 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] > s2[i])
-			return (1);
+			return (15);
 		else if (s1[i] < s2[i])
-			return (-1);
+			return (-15);
 		i++;
 	}
 	/*
@@ -31,7 +31,7 @@ int _strcmp(char *s1, char *s2)
 	if (s1[i] == '\0' && s2[i] == '\0')
 		return (0);
 	else if (s2[i] == '\0')
-		return (1);
+		return (15);
 	else
-		return (-1);
+		return (-15);
 }
