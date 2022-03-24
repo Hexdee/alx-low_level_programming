c97e62fb533c8c21cd078d3a877b36468d219e34
hexdee <lukmansaheed79@gmail.com>
2022-03-24 10:42:44 +0000

    initial commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+
+/**
+ * cap_string - a function that capitalizes
+ * all words of a string.
+ * @s: string to be capitalized
+ * Return: address of s
+ */
+
+char *cap_string(char *s)
+{
+	int i, j, f;
+	char seps[] = {' ', '\t', '\n', ',', ';',
+	     '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	f = 'A' - 'a';
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == seps[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] += f;
+			}
+		}
+	}
+	return (s);
+}
