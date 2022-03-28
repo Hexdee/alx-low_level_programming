9ef844b15d54faab0d81514cb86874fe2d0c5cb8
hexdee <lukmansaheed79@gmail.com>
2022-03-28 21:48:26 +0000

    initial commit

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,22 +1,22 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * print_chessboard - prints the chessboard
+ * print_diagsums - prints the sum of the two
+ * diagonals of a square matrix of integers
  * @a: input pointer.
+ * @size: size of the matrix
  * Return: no return.
  */
-void print_chessboard(char (*a)[8])
+void print_diagsums(int *a, int size)
 {
-	unsigned int i, m = 0;
+	int i, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i < 64; i++)
+	for (i = 0; i < (size * size); i++)
 	{
-		if (i % 8 == 0 && i != 0)
-		{
-			m = i;
-			_putchar('\n');
-		}
-		_putchar(a[i / 8][i - m]);
+		if (i % (size + 1) == 0)
+			sum1 += *(a + i);
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			sum2 += *(a + i);
 	}
-	_putchar('\n');
+	printf("%d, %d\n", sum1, sum2);
 }
