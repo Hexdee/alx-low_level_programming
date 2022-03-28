490f5a3a7e9d86a947df048e43ea5b3ab609d646
hexdee <lukmansaheed79@gmail.com>
2022-03-28 07:44:59 +0000

    initial commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -6,7 +6,6 @@
 */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\"");
-	fprintf(stderr, "- Dora Korpar, 2015-10-19\n", 58);
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
