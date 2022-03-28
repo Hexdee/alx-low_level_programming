5167266077593dbe0f1148a80b0fb38a08e94c43
hexdee <lukmansaheed79@gmail.com>
2022-03-28 12:50:29 +0000

    initial commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,19 +7,21 @@
 
 void print_number(int n)
 {
-	unsigned int i;
+	unsigned int i, m;
 
-	if (n < 0)
+	if (n >= 0)
+		m = n;
+	else
 	{
-		n *= -1;
+		m = n * -1;
 		_putchar('-');
 	}
-	for (i = 1; i * 10 < n; i *= 10)
+	for (i = 1; i * 10 < m; i *= 10)
 		;
 	while (i > 0)
 	{
-		_putchar(n / i + '0');
-		n %= i;
+		_putchar(m / i + '0');
+		m %= i;
 		i /= 10;
 	}
 }
