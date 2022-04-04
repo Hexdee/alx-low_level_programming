f17269e551ec2dd0434b16be9bd18c5f1e347896
hexdee <lukmansaheed79@gmail.com>
2022-04-04 14:45:13 +0000

    fixed bugs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 
 	l1 = s1 == NULL ? 0 : strlen(s1);
 	l2 = s2 == NULL ? 0 : strlen(s2);
-	str = malloc(l1 + l2);
+	str = malloc(l1 + l2 + 1);
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; i < l1; i++)
