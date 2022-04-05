9500ff5dcc63d83b0c47d3379639c2c992a6a9a9
hexdee <lukmansaheed79@gmail.com>
2022-04-05 17:21:18 +0000

    free all memory if malloc fail

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,10 @@ int **alloc_grid(int width, int height)
 	{
 		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
+		{
+			free(grid)
 			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
