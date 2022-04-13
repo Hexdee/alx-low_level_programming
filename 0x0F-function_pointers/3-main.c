8fc2ba133ad5a330e19cd1774e6b7fb2cf50d551
Hexdee <lukmansaheed79@gmail.com>
2022-04-13 16:01:25 +0100

    did task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,44 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: A program that performs simple operations
+ * @argc: Number of cli arguments
+ * @argv: Vector of cli arguments
+ *
+ * Returns: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	int (*op)(int, int);
+	int num1, num2;
+
+	if (argc != 4)
+	{
+		puts("Error");
+		exit(98);
+	}
+
+	if (get_op_func(argv[2]) == NULL)
+	{
+		puts("Error");
+		exit(99);
+	}
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
+	{
+		puts("Error");
+		exit(100);
+	}
+
+	op = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	printf("%d\n", op(num1, num2));
+
+	return (0);
+}	
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+/**
+ * op_add - add two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: addition of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract a number from another
+ * @a: first number
+ * @b: second number
+ *
+ * Return: subtraction of b from a
+ */
+
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * op_mul - multiply two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: multiplication of a and b
+ */
+
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * op_div - divide a number by another
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of a divided by b
+ */
+
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * op_mod - perform modulo operation on two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of a % b
+ */
+
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
