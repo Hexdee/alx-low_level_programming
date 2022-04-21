9bef958003e6807e8681ef9381759143062f33fe
Hexdee <lukmansaheed79@gmail.com>
2022-04-21 16:09:06 +0100

    Fixed betty style error

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,12 +8,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- */
+*/
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
