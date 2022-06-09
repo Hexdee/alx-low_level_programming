b1fe54446f9fde0aca39f2838474214703d4ab8f
hexdee <lukmansaheed79@gmail.com>
2022-06-09 19:07:57 +0000

    Point head to new if head is not empty

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,12 +11,10 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *new = NULL;
 
 	if (!head)
 		return (NULL);
-
-	new = NULL;
 
 	new = malloc(sizeof(dlistint_t));
 
@@ -26,8 +24,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
+	if (*head)
+		(*head)->prev = new;
 	*head = new;
 
 	return (new);
 }
-
