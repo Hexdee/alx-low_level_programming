a49e9c359df6faf8ecec3a9006d76cb270a85ba2
hexdee <lukmansaheed79@gmail.com>
2023-08-21 14:45:40 +0100

    Did dynamic library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Zevrov/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -P /tmp https://github.com/arthurdamm/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libwinner.so
+export LD_PRELOAD=/tmp/libwinner.so
